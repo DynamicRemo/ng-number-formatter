87cc3c24979f07052eeb6558ce2208ef98398ce4
DynamicRemo <dynamicremo@gmail.com>
2020-12-24 20:11:41 +0100

    Handling Negative Numbers, returning negative numbers with Minus Sign

--- src/app/modules/ng-number-formatter/ng-number-formatter.component.ts
+++ src/app/modules/ng-number-formatter/ng-number-formatter.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   inputs: ['ngNumber'],
   template: '<span>{{ngOutputNumber}}</span>',
 })
+
 export class NgNumberFormatterComponent implements OnInit {
   ngNumber: string;
   ngOutputNumber: string;
@@ -12,25 +13,29 @@ export class NgNumberFormatterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var incomingNumber = this.ngNumber;
-    this.ngOutputNumber = this.nFormatter(Math.abs(parseInt(incomingNumber)));
+    const incomingNumber = parseInt(this.ngNumber);
+    if (isNaN(incomingNumber)) {
+      this.ngOutputNumber = (this.ngNumber).toString();
+    } else {
+      this.ngOutputNumber = (incomingNumber > 0) ? '' : '-';
+      this.ngOutputNumber += this.nFormatter(Math.abs(incomingNumber));
+    }
   }
 
   nFormatter(num, digits = 1) {
-    var si = [
+    const si = [
       { value: 1E18, symbol: "E" },
       { value: 1E15, symbol: "P" },
       { value: 1E12, symbol: "T" },
       { value: 1E9, symbol: "B" },
       { value: 1E6, symbol: "M" },
       { value: 1E3, symbol: "K" }
-    ], rx = /\.0+$|(\.[0-9]*[1-9])0+$/, i;
-    for (i = 0; i < si.length; i++) {
+    ], rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    for (let i = 0; i < si.length; i++) {
       if (num >= si[i].value) {
         return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
       }
     }
     return num.toFixed(digits).replace(rx, "$1");
   }
-
 }
